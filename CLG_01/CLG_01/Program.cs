8f6404cb5304ecfdb5f0a5fdf1daaec67e54c071
Johnny Marcelino <johnnyg_engenheiro@hotmail.com>
2022-07-17 00:30:21 -0300

    Challenges 01 and 02 added

---
+++ CLG_01/CLG_01/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+
+namespace CLG_01 {
+    internal class Program {
+        static void Main(string[] args) {
+            // Challenge 01 - 
+
+            // Variable Declaration
+            string product1 = "Computer";
+            string product2 = "Office Table";
+
+            byte age = 30;
+            int code = 5290;
+            char gene = 'M';
+
+            double price = 2100.5;
+            double price1 = 650.50;
+            double mesure = 53.234567;
+
+            // Print results
+            Console.WriteLine("Products: ");
+            Console.WriteLine($"{product1} who the price is: {price}");
+            Console.WriteLine($"{product2} who the price is: {price1}");
+            Console.WriteLine($"Registry: {age} years old, code {code} and the gene: {gene}");
+
+            Console.WriteLine($"Mesure with eight decimal places: {mesure:f8}");
+            Console.WriteLine($"Rounded to three decimal places: {mesure:f3}");
+            Console.WriteLine("Separete decimal palces invariant culture " + mesure.ToString("f2", CultureInfo.InvariantCulture));
+        }
+    }
+}
---
+++ CLG_02/CLG_02/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+namespace CLG_02 {
+    internal class Program {
+        static void Main(string[] args) {
+
+            // Challenge-02: Make a program to perform the following user interactionr,
+            // reading the values highlighted in red and then displaying the datas on the screen.
+            
+
+            // Comands Write, Read, string, double, int, Parse and CultInfo.InvCult
+
+            Console.Write("What is your name ? ");
+            string name = Console.ReadLine();
+            Console.Write("How many bedrooms are there in your house ? ");
+            int bedroom = int.Parse(Console.ReadLine());
+            Console.Write("Enter with any price of a product: ");
+            double priceProd = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Enter with your last name, age and height (on the same line): ");
+            string[] data = Console.ReadLine().Split(" ");
+            string lastName = data[0];
+            int age = int.Parse(data[1]);
+            double tall = double.Parse(data[2]);
+
+            Console.WriteLine($"Your name is: {name}");
+            Console.WriteLine($"You have {bedroom} bedrooms in your house.");
+            Console.WriteLine($"The price of the product that you entered was: {priceProd.ToString("f2", CultureInfo.InvariantCulture)} dollars");
+            Console.WriteLine($"Your last name is: {lastName}");            
+            Console.WriteLine($"You are {age} years old");            
+            Console.WriteLine($"You are {tall.ToString("f2", CultureInfo.InvariantCulture)} meters tall");
+        }
+    }
+}
