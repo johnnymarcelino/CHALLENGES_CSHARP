14aa7a948c095cced27dd0de62e65ab3c85984bb
Johnny Marcelino <johnnyg_engenheiro@hotmail.com>
2022-10-10 12:13:26 -0300

    @johnnymarcelino [challenge 07 done] {ouct.10 at 12:13h}

---
+++ CLG_07/CLG_07/CLG_07/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Globalization;
+
+/*
+CHALLENGE 07:
+Fazer um programa para ler os dados de um funcionário (nome,
+salário bruto e imposto). Em seguida, mostrar os dados do
+funcionário (nome e salário líquido). Em seguida, aumentar o salário
+do funcionário com base em uma porcentagem dada (somente o
+salário bruto é afetado pela porcentagem) e mostrar novamente os
+dados do funcionário. Use a classe projetada abaixo
+*/
+
+
+namespace CLG_07 {
+    internal class Program {
+        static void Main(string[] args) {
+
+            Worker worker = new Worker();
+
+            Console.Write("Nome: ");
+            worker.Name = Console.ReadLine();
+            Console.Write("Gross Salary: ");
+            worker.GrossSalary = double.Parse(Console.ReadLine());
+            Console.Write("Taxes: ");
+            worker.Taxes = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Worker: {worker}");
+
+            Console.Write("Enter with the percentage to increse the salary: ");
+            double salaryIncrese = double.Parse(Console.ReadLine());
+            worker.SalaryIncrese(salaryIncrese);
+
+            Console.WriteLine($"Datas of salary updated: {worker}");
+        }
+    }
+}
---
+++ CLG_07/CLG_07/CLG_07/Worker.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+namespace CLG_07 {
+    internal class Worker {
+
+        public string Name;
+        public double GrossSalary;
+        public double Taxes;
+
+        public double NetSalary() {
+            return GrossSalary - Taxes;
+        }
+
+        public void SalaryIncrese(double increse) {
+            GrossSalary += ((GrossSalary * increse) / 100);
+        }
+
+        public override string ToString() { 
+            return Name + ", $ " + NetSalary().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
