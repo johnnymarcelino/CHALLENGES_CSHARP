555b1b22e253d9886980f0b70455afe96abd6cc4
Johnny Marcelino <johnnyg_engenheiro@hotmail.com>
2022-10-10 16:41:45 -0300

    @johnnymarcelino [challenge 08 done] {ouct.10 at 16:42h}

---
+++ CLG_08/CLG_08/CLG_08/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Globalization;
+
+
+/*
+CHALLENGE 08:
+Fazer um programa para ler o nome de um aluno e as três notas que ele obteve nos três trimestres do ano
+(primeiro trimestre vale 30 e o segundo e terceiro valem 35 cada). Ao final, mostrar qual a nota final do aluno no
+ano. Dizer também se o aluno está APROVADO ou REPROVADO e, em caso negativo, quantos pontos faltam
+para o aluno obter o mínimo para ser aprovado (que é 60 pontos). Você deve criar uma classe Aluno para resolver
+este problema. 
+*/
+
+
+namespace CLG_08 {
+    internal class Program {
+        static void Main(string[] args) {
+
+            Student student = new Student();
+            
+            Console.WriteLine("What is the name of the student ? ");
+            student.Name = Console.ReadLine();
+            Console.WriteLine("Enter with the three grades at the fisrt three trimesters in the school: ");
+            student.Grade1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            student.Grade2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            student.Grade3 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine($"Final Grade is: {student.SumGrades().ToString("F2", CultureInfo.InvariantCulture)}");
+
+            if (student.SumGrades() >= 60.0) {
+                Console.WriteLine("Approved Student!");
+            }
+            else {
+                Console.WriteLine("Reproved Student!");
+                Console.WriteLine($"You need {student.TotalMissing()} points to pass!");
+            }
+        }
+    }
+}
---
+++ CLG_08/CLG_08/CLG_08/Student.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+
+namespace CLG_08 {
+    internal class Student {
+        public string Name;
+        public double Grade1, Grade2, Grade3, GradeTotal;
+
+        public double SumGrades() {
+            //return GradeTotal = ((Grade1 + (Grade1 * (30 / 100))) + (Grade2 + (Grade2 * (35 / 100))) + (Grade3 + (Grade3 * (35 / 100)))) / 3; => lógica direta, somente utilizando uma função.
+            return Grade1 + Grade2 + Grade3;
+        }
+
+        public bool Approved() {
+            if (SumGrades() >= 60.0) {
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+
+        public double TotalMissing() {
+            if (Approved()) {
+                return 0.0;
+            }
+            else { 
+                return 60.0 - SumGrades(); 
+            }
+        }
+    }
+}
