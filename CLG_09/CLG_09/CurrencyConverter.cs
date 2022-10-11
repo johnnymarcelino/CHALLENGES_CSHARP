4bc3483bafa3c301d75247a320883f3bc85ef03f
Johnny Marcelino <johnnyg_engenheiro@hotmail.com>
2022-10-11 10:22:31 -0300

    @johnnymarcelino [Challenge CLG_09 done] {ouct.11 at 10:22h}

---
+++ CLG_09/CLG_09/CurrencyConverter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CLG_09 {
+    internal class CurrencyConverter {
+
+        public static double Iof = 6.0;
+
+        public static double DollarToReal(double quantity, double price) { 
+            double total = quantity * price;
+            return total + total * Iof / 100.0;
+        }
+    }
+}
---
+++ CLG_09/CLG_09/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+
+/*
+CHALLANGE 09:
+Fazer um programa para ler os valores da largura e altura de um retângulo. Em
+seguida, mostrar na tela o valor de sua área, perímetro e diagonal. Usar uma classe
+como mostrado no projeto ao lado. 
+*/
+
+namespace CLG_09 {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.Write("What is it the price of the dollar: ");
+            double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("How much do you intend to buy at the mall: ");
+            double quantity = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double result = CurrencyConverter.DollarToReal(price, quantity);
+
+            Console.WriteLine($"Value to pay in real = " + result.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
