aa563a855feb1298169c442b4dea7e71c6602307
Johnny Marcelino <johnnyg_engenheiro@hotmail.com>
2022-10-12 12:44:52 -0300

    @johnnymarcelino [Challenge 10 done] {ouct.12 at 12:44h}

---
+++ CLG_10/CLG_10/BankOOP.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace CLG_10 {
+    internal class BankOOP {
+
+        public string Name { get; set; }
+        public double Token { get; set; }
+        public double _deposit { get; set; }
+
+        public BankOOP() {
+            _deposit = 0.0;
+        }
+
+        public void DepoYes(string startDep) {
+            if (startDep.ToUpper() == "Y") {
+                double d = 0.0;
+                Console.Write("Enter with a value to deposit: ");
+                d = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                _deposit += d;
+            }
+            else {
+                Console.WriteLine("You have chosen don't realize a account deposit!");
+            }
+        }
+
+        //public void DepoNo(string startDep) {
+        //    if (startDep == )
+
+        //public double DepoStart() {
+        //    double d = 0.0;
+        //    Console.Write("Enter with a value to deposit: ");
+        //    d = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        //    return _deposit += d;
+        //    //double depoStart = double.Parse(Console.ReadLine());
+        //}
+
+        public void Withdraw(string withdraw) {
+            if (withdraw.ToUpper() == "Y") {
+                double wd = 0.0;
+                Console.WriteLine("Warning! If you realize a withdraw, you have a tax of $ 5.00 dollars.");
+                Console.Write("Enter with a value to do a withdraw: ");
+                wd = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                _deposit -= wd + 5.00;
+            }
+            else {
+                Console.WriteLine("You have chosen don't realize a cash withdraw!");
+            }
+        }
+
+        public override string ToString() {
+            return "Account Token: " + Token + ", Owner: " + Name + ", Balance: " + _deposit.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ CLG_10/CLG_10/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace CLG_10 {
+    internal class Program {
+        static void Main(string[] args) {
+
+            BankOOP owner = new BankOOP();
+
+            Console.Write("Enter with the number of your account: ");
+            owner.Token = double.Parse(Console.ReadLine());
+
+            Console.Write("Enter with the name of the account owner: ");
+            owner.Name = Console.ReadLine();
+
+            Console.Write("Will be deposit account at the beginning ? (y / n) ");
+            string depoYesNot = Console.ReadLine();
+            owner.DepoYes(depoYesNot);
+            //owner._deposit = Console.ReadLine();
+
+            Console.WriteLine($"Account data:");
+            Console.WriteLine($"{owner}");
+
+            Console.WriteLine("Would you like to deposit any more ? (y / n)");
+            depoYesNot = Console.ReadLine();
+            owner.DepoYes(depoYesNot);
+
+            Console.WriteLine($"Account data updated:");
+            Console.WriteLine($"{owner}");
+            
+            Console.WriteLine($"Would you like to do a withdraw ? (y / n)");
+            string withdrawYesNot = Console.ReadLine();
+            owner.Withdraw(withdrawYesNot);
+
+            Console.WriteLine($"Account data updated:");
+            Console.WriteLine($"{owner}");
+
+
+        }
+    }
+}
