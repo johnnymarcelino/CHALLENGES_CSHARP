77aa197aede22fc52eb761b0c6f0fbb47674d6a3
Johnny Marcelino <johnnyg_engenheiro@hotmail.com>
2022-10-24 16:29:52 -0300

    @johnnymarcelino [Challenge 16 (Composition2) done] {oct.24.2022 at 16:30pm}

---
+++ CLG_16/Composition2/Entities/Post.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Composition2.Entities
+{
+    internal class Post
+    {
+        // The attribuits
+
+        public DateTime Moment { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public int Likes { get; set; }
+        public List<Comment> Comments { get; set; } = new List<Comment>();  // to make sure the instantiation of the list
+
+        // defaul constructor that don't get paramenters
+        public Post()
+        {
+        }
+
+        // constructor that will receive paramenters
+        public Post(DateTime dateTime, string title, string content, int likes)
+        {
+            Moment = dateTime;
+            Title = title;
+            Content = content;
+            Likes = likes;
+        }
+
+        public void AddComments(Comment comment)
+        {
+            Comments.Add(comment);  // attribuits, paraments and variable 
+        }
+        
+        public void RemoveContent(Comment comment)
+        {
+            Comments.Remove(comment);  // attribuits, paraments and variable 
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine(Title);
+            sb.Append(Likes);
+            sb.Append(" Likes - ");
+            sb.AppendLine(Moment.ToString("dd/MM/yyyy HH:mm:ss"));
+            sb.AppendLine(Content);
+            sb.AppendLine("Comments:");
+            foreach (Comment c in Comments)
+            {
+                sb.AppendLine(c.Text);
+            }
+            return sb.ToString();
+        }
+    }
+}
---
+++ CLG_16/Composition2/Program.cs
@@ -0,0 +1,36 @@
+﻿using Composition2.Entities;
+using System;
+using System.ComponentModel;
+
+namespace Composition2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Comment c1 = new Comment("Have a nice trip!");
+            Comment c2 = new Comment("Wow! that's awesome!");
+            Post p1 = new Post(
+                DateTime.Parse("21/06/2018 13:05:44"),
+                "Traveling to the New Zealand",
+                "I'm going to visit this wonderful country!",
+                12);
+            p1.AddComments(c1);
+            p1.AddComments(c2);
+
+            Comment c3 = new Comment("Good night");
+            Comment c4 = new Comment("May the force be will you");
+            Post p2 = new Post(
+                DateTime.Parse("28/07/2018 23:14:19"),
+                "Good night guys",
+                "See you tomorrow",
+                5);
+
+            p2.AddComments(c3);
+            p2.AddComments(c4);
+
+            Console.WriteLine(p1);
+            Console.WriteLine(p2);
+        }
+    }
+}
