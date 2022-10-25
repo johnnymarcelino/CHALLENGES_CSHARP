76a21c32fa504322f0c7eff31f2e3ff6a7de8526
Johnny Marcelino <johnnyg_engenheiro@hotmail.com>
2022-10-25 11:04:05 -0300

    @johnnymarcelino [Challenge 17 done] {oct.25.2022 at 11:04am}

---
+++ CLG_17/CLG_17/Program.cs
@@ -0,0 +1,53 @@
+﻿using CLG_17.Entities;
+using CLG_17.Entities.Enums;
+using System;
+using System.Globalization;
+
+namespace CLG_17
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter client data:");
+            Console.Write("Name: ");
+            string clientName = Console.ReadLine();
+            Console.Write("Email: ");
+            string email = Console.ReadLine();
+            Console.Write("Birth date (DD/MM/YYYY): ");
+            DateTime birthDate = DateTime.Parse(Console.ReadLine());
+            Console.WriteLine("Enter order data: ");
+            Console.Write("Status: ");
+            OrderStatus status = Enum.Parse<OrderStatus>(Console.ReadLine());
+
+            Client client = new Client(clientName, email, birthDate);
+            Order order = new Order(DateTime.Now, status, client);
+
+            Console.Write("How many items to this product: ");
+            int items = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= items; i++)
+            {
+                Console.WriteLine($"Enter #{i}item data: ");
+                Console.WriteLine("Product name: ");
+                string productName = Console.ReadLine();
+                Console.WriteLine("Product price: ");
+                double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                Product product = new Product(productName, price);
+
+                Console.WriteLine("Quantity: ");
+                int quant = int.Parse(Console.ReadLine());
+
+                OrderItem orderItem = new OrderItem(quant, price, product);
+
+                order.AddItem(orderItem);
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("ORDER SUMMARY: ");
+            Console.WriteLine(order);
+        }
+    }
+}
