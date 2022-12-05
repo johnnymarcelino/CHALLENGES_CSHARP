66041a78396a0722ac0966c667e60bc2c58da412
Johnny Marcelino <johnnyg_engenheiro@hotmail.com>
2022-12-04 23:25:15 -0300

    @johnnymarcelino [Challenge 25 - Collections started] {dec.04.2022 11:25pm}

---
+++ CLG_25/CLG_25/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+namespace CLG_25
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter file full path: ");
+            string path = Console.ReadLine();
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string line = sr.ReadLine();
+                        Console.WriteLine(line);
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
