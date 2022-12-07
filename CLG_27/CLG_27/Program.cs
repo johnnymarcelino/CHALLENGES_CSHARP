c7dd222ff6abc06e9451e59810fccc45c6575ee4
Johnny Marcelino <johnnyg_engenheiro@hotmail.com>
2022-12-07 12:07:07 -0300

    @johnnymarcelino [Challenge 27 - done] {dec.07.2022 12:07pm}

---
+++ CLG_27/CLG_27/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace CLG_27
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter file full path: ");
+            string path = Console.ReadLine();
+
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    Dictionary<string, int> dictionary = new Dictionary<string, int>();
+
+                    while (!sr.EndOfStream)
+                    {
+                        string[] votingRecord = sr.ReadLine().Split(',');
+                        string candidate = votingRecord[0];
+                        int votes = int.Parse(votingRecord[1]);
+
+                        if (dictionary.ContainsKey(candidate))
+                        {
+                            dictionary[candidate] += votes;
+                        }
+                        else
+                        {
+                            dictionary[candidate] = votes;
+                        }
+                    }
+                    foreach (KeyValuePair<string, int> pair in dictionary)
+                    {
+                        Console.WriteLine(pair.Key + ": " + pair.Value);
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred!");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
