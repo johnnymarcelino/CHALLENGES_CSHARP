93c9a53a249036c889a058376035df227e5ac763
Johnny Marcelino <johnnyg_engenheiro@hotmail.com>
2022-12-07 08:52:54 -0300

    @johnnymarcelino [Challenge 26 - started (in process)] {dec.07.2022 8:53am}

---
+++ CLG_26/CLG_26/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+
+/*
+CHALLENGE 26:
+Em um portal de cursos online, cada usuário possui um código único, representado por um número inteiro.
+Cada instrutor do portal pode ter vários cursos, sendo que um mesmo aluno pode se matricular em quantos
+cursos quiser. Assim, o número total de alunos de um instrutor não é simplesmente a soma dos alunos de 
+todos os cursos que ele possui, pois pode haver alunos repetidos em mais de um curso.
+O instrutor Alex possui três cursos A, B e C, e deseja saber seu número total de alunos. Seu programa
+deve ler os alunos dos cursos A, B e C do instrutor Alex, depois mostrar a quantidade total e alunos dele.
+*/
+
+
+namespace CLG_26
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
